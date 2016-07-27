4ea8b6278d4ba04e82c13cfd116fcf876fd4a0fc
hezhiying <21418592@qq.com>
2016-07-27 19:22:53 +0800

    add Tree Class

--- src/Tree.php
+++ src/Tree.php
@@ -12,4 +12,57 @@ namespace zine;
 class Tree
 {
 
+    /**
+     * tree 树形数据转换成rows
+     * @param $array
+     * @return array
+     */
+    public static function treeToArray( $array )
+    {
+        $arr = [];
+        $tem = [];
+        foreach ($array as $item) {
+            $arr[] = $item;
+
+            if($item['children']){
+                $children = self::treeToArray( $item['children'] );
+                foreach ($children  as $v) {
+                    $arr[] = $v;
+                }
+            }
+        }
+        return $arr;
+    }
+
+    /**
+     * 将rows转换成tree结构
+     * @param array $array
+     * @param int   $pid
+     * @param int   $lev
+     * @return array
+     * 返回数据结构
+     * {
+     *  id:1,
+     *  pid:0,
+     *  level:0
+     *  children:[]
+     * }
+     */
+    public static function initData($array = [], $pid = 0, $lev=0){
+        $arr = array();
+        $tem = array();
+        foreach ($array as $v) {
+            if ($v['pid'] == $pid) {
+                $tem = self::initData($array, $v['id'], $lev+1);
+                //tree level
+                $v['level'] = $lev;
+                //tree children
+                $v['children'] = $tem;
+                $arr[] = $v;
+            }
+        }
+        return $arr;
+    }
+
+
 }
